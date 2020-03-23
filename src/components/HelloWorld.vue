ad25997fb608450e24b6339c40af02f05fe5cdce
David <stickdave2003@gmail.com>
2020-03-23 17:51:29 +0100

    initial commit of basic preferences store

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    <h1>Preferances</h1>
+    <p v-if="dataLoaded==false">
+    Loading 
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p v-if="dataSaving==true">
+    Saving
+    </p>
+    <div v-if="dataLoaded==true">
+    <p>
+      There are {{ nbMailItems }} allowed
+    </p>
+    <p>
+    <input v-model="nbMailItemsInput" placeholder="number of allowed" type="number">    
+    </p>
+    <p>
+      Track and Trace for {{ ttNumberOfDays }} days
+    </p>
+    <p>
+    <input v-model="nbDaysInput" placeholder="number of days" type="number">    
+    </p>
+    <button v-if="dataSaving==false"
+      @click="save">
+      Save
+    </button>
+  </div>
   </div>
 </template>
 
@@ -34,8 +32,55 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+  },
+  data() {
+    return {
+      nbDaysInput: null,
+      nbMailItemsInput: null,
+      dataLoaded:false,
+      dataSaving:false,
+    }
+  },
+  computed: {
+    nbMailItems () {
+      return this.$store.state.nbMailItems
+    },
+    ttNumberOfDays () {
+      return this.$store.state.ttNumberOfDays
+    },
+  },
+  methods: {
+    save() {
+      console.log("save data")
+      this.dataSaving = true;
+      // Dispatch the action to save config data
+      //this.$store.dispatch('SAVE_nBMailItems',{nbMailItems : +this.nbMailItemsInput, ttNumberOfDays : +this.nbDaysInput})
+      this.$store.dispatch('SAVE',{nbMailItems : +this.nbMailItemsInput, ttNumberOfDays : +this.nbDaysInput}).then(response => {
+          console.log("Saved datadata " +response.data.nbMailItems)
+        this.dataSaving = false;
+      }, error => {
+            console.error("Got nothing from server. Prompt user to check internet connection and try again" + error) 
+        })
+    },
+  },
+  mounted(){
+    console.log("mounted... get nbMailitms")
+
+       // This component just got created. Lets fetch some data here using an action
+        // the action execution is asynchronous so we programmed it to return a promise.
+        // This allows us to set the initial input values when the promise returns
+        // we could also use this to preent a "loading ..." message
+        this.$store.dispatch("GET_nBMailItems").then(response => {
+          console.log("Got some data, now lets show something in this component " +response.data.nbMailItems)
+          this.nbMailItemsInput = response.data.nbMailItems
+          this.nbDaysInput = this.ttNumberOfDays
+          this.dataLoaded = true
+        }, error => {
+            console.error("Got nothing from server. Prompt user to check internet connection and try again" + error) 
+        })
+  
+   }
+
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,90 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    nbMailItems: null,
+    ttNumberOfDays: null,
   },
+  // Mutations change the state. They are called by actions
+  // They are synchronous cannot run ascynchronous calls (Axios callse to API)
   mutations: {
+    SET_nbMailItems : (state,payload) => {
+      state.nbMailItems = payload.numMailitems
+      state.ttNumberOfDays = payload.ttNumberOfDays
+    },
   },
+  // Actions are called from the GUI
+  // They are not synchronous - which means they are asynchronous!
+  // So they return immediately. Internally we make it return a promise
+  // so that we know when the data has loaded
   actions: {
+    GET_nBMailItems(context) {
+      return new Promise((resolve, reject) => {
+          // Do something here... lets say, a http call using vue-resource
+          axios.get ("https://localhost:5001/api/config")
+          .then(response => {
+              // http success, call the mutator and change something in state
+              console.log(response);
+              var numMailitems = response.data.nbMailItems
+              var ttNumberOfDays = response.data.ttNumberOfDays
+              // use mutation to set config in the state
+              context.commit('SET_nbMailItems',{numMailitems : numMailitems, ttNumberOfDays: ttNumberOfDays})
+              resolve(response);  // Let the calling function know that http is done. You may send some data back
+          }, error => {
+              // http failed, let the calling function know that action did not work out
+              reject(error);
+          })
+      })
+  },
+  SAVE(context, payload) {
+    return new Promise((resolve, reject) => {
+        // Do something here... lets say, a http call using vue-resource
+        axios.put ("https://localhost:5001/api/config",
+        {
+          NbMailItems: payload.nbMailItems,
+          EventListTracked: "1, 2, 3, 4, 5, 6, 7",
+          PathToStoreSignatureImg: "",
+          TTNumberOfDays: payload.ttNumberOfDays
+      })
+        .then(response => {
+            // http success, call the mutator and change something in state
+            console.log(response);
+            context.commit('SET_nbMailItems', { numMailitems : payload.nbMailItems, ttNumberOfDays : payload.ttNumberOfDays})
+            resolve(response);  // Let the calling function know that http is done. You may send some data back
+        }, error => {
+            // http failed, let the calling function know that action did not work out
+            reject(error);
+        })
+    })
+},
+
+   // Update the config value from the gui
+   SAVE_nBMailItems : async (context, payload) => {
+      // Post the value to the DB
+      /*let { data } = */await axios.put('http://localhost:5000/api/config',
+      {
+        NbMailItems: payload.nbMailItems,
+        EventListTracked: "1, 2, 3, 4, 5, 6, 7",
+        PathToStoreSignatureImg: "",
+        TTNumberOfDays: payload.ttNumberOfDays
+    })
+      // use mutation to set config in the state
+      context.commit('SET_nbMailItems', { numMailitems : payload.nbMailItems, ttNumberOfDays : payload.ttNumberOfDays})
+   },
   },
   modules: {
-  }
+  },
+  getters: {
+    nbMailItems : state => {
+      return state.nbMailItems
+    },
+    ttNumberOfDays : state => {
+      return state.ttNumberOfDays
+    },
+
+  },
 })
